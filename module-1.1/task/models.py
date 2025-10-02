55f570ec4dfa100ab5d72b7053deb902e5d13ea0
meherun05 <maherunn.cmt@gmail.com>
2025-10-03 00:06:07 +0600

    add postgres sql

--- module-1.1/task/models.py
+++ module-1.1/task/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length= 250)
+    description = models.TextField()
+    dueDate = models.DateField()
+    isCompleted = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+class TaskDetails(models.Model):
+    HIGH = 'H'
+    MEDIUM = 'M'
+    LOW = 'L'
+    PRIORITY_OPTION = (
+        (HIGH, 'High'),
+        (MEDIUM, 'Medium'),
+        (LOW, 'Low'),
+    )
+    task = models.OneToOneField(Task, on_delete= models.CASCADE)
+    assingeTo = models.CharField(max_length=100)
+    priority = models.CharField(max_length= 1,choices=PRIORITY_OPTION,default=LOW )
